21bfd7625f61b9536ba1888817b5bf939f6edff7
IsNotAvailableName <148902549+IsNotAvailableName@users.noreply.github.com>
2023-12-12 18:45:41 +0300

    Done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -1,11 +1,23 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
-    public void printPyramid(int cathetusLength) {
-
+    public static void main(String[] args) {
+        new Pyramid().printPyramid(10);
     }
 
-    public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+    public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            int currentInt = i;
+            for (int j = 1; j < i + 1; j++) {
+                System.out.print(--currentInt + 1);
+            }
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print(++currentInt + 1);
+            }
+            System.out.println();
+        }
     }
 }
